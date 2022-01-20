2374dcf2cbe249ca4cec5c965f90f67f69b1bfcc
Haeun Kim <haeun159951@gmail.com>
2022-01-20 14:34:07 -0500

    built breaking bad cast app using api

---
+++ src/components/spinner/spinner.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import spinner from "../../img/spinner.gif";
+
+const Spinner = () => {
+  return (
+    <div className="center">
+      <img src={spinner} alt="" />
+    </div>
+  );
+};
+export default Spinner;
